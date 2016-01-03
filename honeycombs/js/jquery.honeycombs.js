e12085b5fa25d89dab2edea02c5f36606489c8c9
smrubin <sam.rubin@comcast.net>
2016-01-02 19:24:19 -0500

    Honeycombs Vertical & Horizontal init
    Forked from Tiotao’s honeycombs. Reworked jQuery honeycombs function
    math and javascript. Added option for user to set orientation of the
    combs to horizontal.

--- honeycombs/js/jquery.honeycombs.js
+++ honeycombs/js/jquery.honeycombs.js
@@ -1,29 +1,25 @@
-(function($) {
+(function($){
 
-    $.fn.honeycombs = function(options) {
+    $.fn.honeycombs = function(options){
 
-        // Establish our default settings
         var settings = $.extend({
             combWidth: 250,
             margin: 0,
-            threshold: 3
+            horizontal: false
         }, options);
 
-        function initialise(element) {
+        function initialize(element){
             
             $(element).addClass('honeycombs-wrapper');
             
-            var width = 0;
-            var combWidth = 0;
-            var combHeight = 0;
-            var num = 0;
-            var $wrapper = null;
+            var width = 0;          // Width of the honeycomb
+            var combWidth = 0;      // Width of a single comb, measured from corner to corner
+            var combHeight = 0;     // Height of a single comb, set equal to combWidth
+            var $wrapper = null;    // Honeycomb inner-wrapper
             
-            /**
-             * Build the dom
-             */
+            /* Build the dom. */
             function buildHtml(){
-                // add the 2 other boxes
+
                 $(element).find('.comb').wrapAll('<div class="honeycombs-inner-wrapper"></div>');
                 $wrapper = $(element).find('.honeycombs-inner-wrapper');
 
@@ -31,11 +27,8 @@
                 $(element).find('.comb').append('<div class="inner back"></div>');
                 $(element).find('.inner').append('<div class="wrapper"></div>');
                 $(element).find('.comb').append('<span class="icon-hex-lg"></span>');
-
-                num = 0;
                 
                 $(element).find('.comb').each(function(){
-                    num = num + 1;
 
                     if($(this).find('.inner').length > 0){
                         $(this).find('.inner.front .wrapper').html($(this).find('.front-content').html());
@@ -44,18 +37,16 @@
                         $(this).find('.back-content').remove();
                     }else{
                         $(this).find('.inner').remove();
-                    };
+                    }
                 });
 
-                // Fix Firefox padding error
-                if (navigator.userAgent.search("Firefox") > -1) { 
-                    $('.comb span').addClass('firefox');
+                // Rotate hexagon
+                if (settings.horizontal == true) {
+                    $('.comb span').addClass('horizontal');
                 }
             }
             
-            /**
-             * Update all scale values
-             */
+            /* Update all scale values */
             function updateScales(){
                 combWidth = settings.combWidth;
                 combHeight = combWidth;
@@ -63,110 +54,114 @@
                 $(element).find('.icon-hex-lg').css('font-size', combWidth)
             }
             
-            /**
-             * update css classes
-             */
+            /* Update honeycomb layout */
             function reorder(animate){
                 
                 updateScales();
                 width = $(element).width();
-                
-                newWidth = $('.honeycombs').parent().width();
-                
-                
-                if(newWidth < width){
-                    width = newWidth;
-                }
-                
-                $wrapper.width(newWidth);
 
-                var maxLeft = 0;
-                var row = 0; // current row
-                var offset = 0; // 1 is down
-                var left = 1; // pos left
-                var top = 0; // pos top
-                var cols = 0;
+                var maxLeft = 0;                    // Max left positioning for honeycomb, increases as combs are added
+                var row = 0;                        // Current row of honeycomb
+                var topHorizontalRow = true;        // If horizontal is selected, indicates if comb is in the top row
+                var left = 0;                       // Left positioning of a comb
+                var top = 0;                        // Top positioning of a comb
 
-                var noOffset = function(offset){
-                    return offset
+                /* Hexagon apothem is distance from center to midpoint of any side. 
+                Used as top-handler for a horizontal layout and the left-handler for a vertical layout. */
+                var apothem = function(){
+                    return ( 0.5 * combWidth * 0.5 * Math.sqrt(3) );
                 };
 
-                var withOffset = function(offset){
-                    return (offset + 1) % 2;
-                }
+               /* Three quarters of the maximum distance across the hexagon. 
+                Used as top-handler for a vertical layout and the left-handler for a horizontal layout. */
+                var threeQuartersCombWidth = function(){
+                    return ( 0.75 * combWidth );
+                };
 
-                var halfTop = function(top){
-                    return ( row * ( 0.5 * combHeight * Math.sqrt(3) + settings.margin) )
-                }
+                function orderCombs(topHandler){
 
-                var fullTop = function(top){
-                    return ( row * (combHeight + settings.margin + combHeight * 0.1))
-                }
+                    $(element).find('.comb').each(function(index){
 
-                function orderCombs(leftHandler, topHandler){
-
-                    $(element).find('.comb').filter(':not(.placeholder.hide)').each(function(index){
+                        top = row * ( topHandler() + settings.margin );     // Set top position for current comb
                         
-                        top = topHandler(top);
-
                         if(animate == true){
                             $(this).stop(true, false);
                             $(this).animate({'left': left, 'top': top});
                         }else{
                             $(this).css('left', left).css('top', top);
                         }
-                        
-                        left = left + ( combWidth + settings.margin );
-                        
-                        if(left > maxLeft) {
-                            maxLeft = left;
-                        }
-                       
-                        
-                        if(row == 0){
-                            cols = cols + 1;
-                        }
+
+                        /* Horizontal Layout */
+                        if(settings.horizontal == true){
                             
-                        if(left + combWidth > width){
-                            row = row + 1;
-                            offset = leftHandler(offset);
-                            left = offset / 2 * ( combWidth + settings.margin ) ;
+                            /* For the first row in a horizontal layout, the comb must maintain shape. Therefore, the combs must be laid out differently
+                            for the first row until we reach the element width */
+                            if(topHorizontalRow == true){
+
+                                if(row != 0){   // Only increment left position when not the top row
+                                    left = left + threeQuartersCombWidth() + settings.margin;
+                                }
+
+                                if(left > maxLeft) {
+                                    maxLeft = left + 0.25 * combWidth;
+                                }
+
+                                row = ( index % 3 == 0 ? 2 : ( index % 3 == 1 ? 1 : 0) );   // Determines row for the next comb based on current index
+
+                                if(left + combWidth > width){
+                                    topHorizontalRow = false;
+                                    row = 3;
+                                    left = (row % 2) * ( threeQuartersCombWidth() + settings.margin );
+                                }
+                            }
+                            /* After the first horizontal row, add combs to empty bottom spaces */
+                            else{
+                                left = left + 2 * ( threeQuartersCombWidth() + settings.margin );
+
+                                if(left + combWidth > width){
+                                    row = row + 1;
+                                    left = (row % 2) * ( threeQuartersCombWidth() + settings.margin );
+                                }
+                            }
                         }
 
+                        /* Vertical Layout */
+                        else{
+                            left = left + ( 2 * apothem() + settings.margin );
+                            
+                            if(left + 0.5 * ( combWidth - 2 * apothem() ) > maxLeft) {
+                                maxLeft = left + 0.5 * ( combWidth - 2 * apothem() );
+                            }
+                            
+                            if(left + combWidth > width){
+                                row = row + 1;
+                                left = (row % 2) / 2 * ( 2 * apothem() + settings.margin );
+                            }
+                        }
                     });
                 }
 
-
-                if (newWidth < 1.5 * (combWidth + settings.margin)) {
-                    $('.comb.placeholder').addClass('hide');
-
-                    orderCombs(noOffset, fullTop);
-                } else if (newWidth < settings.threshold * (combWidth + settings.margin)) {
-                    $('.comb.placeholder').addClass('hide');
-                    orderCombs(withOffset, halfTop);
-                } else {
-                    $('.comb.placeholder').removeClass('hide');
-                    orderCombs(withOffset, halfTop);
+                if(settings.horizontal == true){
+                    orderCombs(apothem);
+                }
+                else{
+                    orderCombs(threeQuartersCombWidth);
                 }
                 
-                
-                $wrapper
-                    .height(top + combHeight)
-                    .width(maxLeft - settings.margin)
+                $wrapper.height(top + combHeight).width(maxLeft + settings.margin);
             }
             
             $(window).resize(function(){
                 reorder(true);
             });
-            
+
             buildHtml();
             reorder(false);
         }
 
         return this.each(function() {
-            initialise(this);
+            initialize(this);
         });
-
     }
 
-}(jQuery));
+})(jQuery);
